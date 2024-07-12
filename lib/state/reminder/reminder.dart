3dc5537c0c0f32f1ae19ca043e1f706848734eb5
1999Alain <fonkousainclair@yahoo.fr>
2024-07-12 12:53:23 +0100

    first commit

---
+++ lib/state/reminder/reminder.dart
@@ -0,0 +1,39 @@
+import 'package:mobx/mobx.dart';
+
+part 'reminder.g.dart';
+
+class Reminder = _Reminder with _$Reminder;
+
+abstract class _Reminder with Store {
+  final String id;
+  final DateTime creationDate;
+
+  @observable
+  String text;
+
+  @observable
+  bool isDone;
+
+  _Reminder({
+    required this.id,
+    required this.creationDate,
+    required this.text,
+    required this.isDone,
+  });
+
+  @override
+  bool operator ==(covariant _Reminder other) {
+    return id == other.id &&
+        text == other.text &&
+        isDone == other.isDone &&
+        creationDate == other.creationDate;
+  }
+
+  @override
+  int get hashCode => Object.hash(
+        id,
+        text,
+        isDone,
+        creationDate,
+      );
+}
